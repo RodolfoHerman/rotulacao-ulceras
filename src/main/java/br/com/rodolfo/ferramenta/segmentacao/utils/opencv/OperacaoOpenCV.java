fba0ccccfeac42c480fb7c7f44ac18d2d9fd1b20
Rodolfo Herman <ciencia.rodolfo@gmail.com>
2019-03-23 13:19:24 -0300

    Criação do método calcularArea

--- src/main/java/br/com/rodolfo/ferramenta/segmentacao/utils/opencv/OperacaoOpenCV.java
+++ src/main/java/br/com/rodolfo/ferramenta/segmentacao/utils/opencv/OperacaoOpenCV.java
@@ -1,5 +1,8 @@
 package br.com.rodolfo.ferramenta.segmentacao.utils.opencv;
 
+import org.bytedeco.javacpp.indexer.UByteRawIndexer;
+import org.bytedeco.javacpp.opencv_core.Mat;
+
 /**
  * OperacaoOpenCV
  */
@@ -9,5 +12,35 @@ public class OperacaoOpenCV {
      * Não permitir instânciação da classe
      */
     private OperacaoOpenCV() {}
+
+    /**
+     * Calcula a área da figura binária
+     * 
+     * @param imagem
+     * @param comparador
+     * @return área
+     */
+    public static double calcularArea(Mat imagem, int comparador) {
+        
+        double area = 0.0;
+
+        if(imagem.depth() <= 1) {
+
+            UByteRawIndexer indice = imagem.createIndexer();
+            int pixels = imagem.rows() * imagem.cols();
+
+            for(int x = 0; x < pixels; x++) {
+                
+                if(indice.get(x) == comparador) {
+
+                    area++;
+                }
+            }
+
+            indice.release();
+        }
+
+        return area;
+    }
     
 }
